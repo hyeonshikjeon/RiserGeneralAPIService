bf61fe194af554f37cde11fecdf6f98bbcd51760
Hyeon Shik Jeon <hyeonshikjeon@gmail.com>
2023-07-04 22:15:03 -0700

    correcting db string. possible solution: 3

--- project/db/base.py
+++ project/db/base.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://general_dbu:general_dbp@localhost:3320/general_db"
+DATABASE_URL = "postgresql://general_dbu:general_dbp@localhost:5432/general_db"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(
