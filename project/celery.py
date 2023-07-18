1c1d18bc9cd81698133b6da07b4b79ae71f63fb6
Hyeon Shik Jeon <hyeonshikjeon@gmail.com>
2023-07-17 22:28:37 -0700

    correcting db string. issue solved.

--- project/celery.py
+++ project/celery.py
@@ -1,6 +1,13 @@
 from celery import Celery
 
-celery_app = Celery('project',
-                    broker="amqp://general:general123@localhost:5672/general_host",
-                    backend='rpc://',
-                    include=['project.app'])
+app = Celery('project',
+             broker='amqp://riser_user:riser_pw@localhost:5672//',
+             backend='rpc://',
+             include=['project.general_tasks'])
+
+app.conf.update(
+    result_expires=3600
+)
+
+if __name__ == '__main__':
+    app.start()
